c7d6f51dfaecc580fdc9f752e538b90dfc13a361
Mouchi <grosjen@hotmail.fr>
2015-09-19 12:26:11 +0200

    Add a try/catch in the main, to catch all the errors

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,5 +1,7 @@
 package base;
 
+import javax.swing.JOptionPane;
+
 import config.ChooseLanguage;
 import config.ConfigStorage;
 import config.WorkingSession;
@@ -14,47 +16,68 @@ import text.TextFrancais;
  *
  */
 public class Main {
-	public static void main(String[] args) {	
-		// Language define by the script
-		Text defaultText;
-		if (args.length > 0 && args[0].equals("-en")) {
-			defaultText = new TextEnglish();
-		} else {
-			defaultText = new TextFrancais();
-		}
-
-		// Gather the configuration file
-		final String configurationFile = "ressources/config.xml";
-		ConfigStorage configuration = new ConfigStorage(configurationFile);
-		Text text = configuration.getText();
-
-		if (text == null) {
-			// The configuration is invalid (or don't exist)
-			// Language define by the user
-			ChooseLanguage languageChoiceDialog = new ChooseLanguage(null, true,
-					defaultText);
-			text = languageChoiceDialog.getText();
-			if (text == null) {
-				// The user cancels
-				languageChoiceDialog.dispose();
-				throw new IllegalArgumentException(defaultText.missingLanguage());
+	public static void main(String[] args) {
+		Window window = null;
+		try {
+			// Language define by the script
+			Text defaultText;
+			if (args.length > 0 && args[0].equals("-en")) {
+				defaultText = new TextEnglish();
+			} else {
+				defaultText = new TextFrancais();
 			}
 
-			// Create the configuration file
-			configuration = new ConfigStorage(text, configurationFile);
-			configuration.saveConfigFile();
+			// Gather the configuration file
+			final String configurationFile = "ressources/config.xml";
+			ConfigStorage configuration = new ConfigStorage(configurationFile);
+			Text text = configuration.getText();
 
-			// Create an empty window
-			new Window(text, configuration);
-		} else {
-			if (configuration.hasWorkingSession()) {
-				// Try to create a window with the first working session
-				WorkingSession ws;
-				ws = configuration.getFirst();
-				new Window(ws, text, configuration);
-			} else {
+			if (text == null) {
+				// The configuration is invalid (or don't exist)
+				// Language define by the user
+				ChooseLanguage languageChoiceDialog = new ChooseLanguage(null, true,
+						defaultText);
+				text = languageChoiceDialog.getText();
+				if (text == null) {
+					// The user cancels
+					languageChoiceDialog.dispose();
+					throw new IllegalArgumentException(defaultText.missingLanguage());
+				}
+
+				// Create the configuration file
+				configuration = new ConfigStorage(text, configurationFile);
+				configuration.saveConfigFile();
+
 				// Create an empty window
-				new Window(text, configuration);
+				window = new Window(text, configuration);
+			} else {
+				if (configuration.hasWorkingSession()) {
+					// Try to create a window with the first working session
+					WorkingSession ws;
+					ws = configuration.getFirst();
+					window = new Window(ws, text, configuration);
+				} else {
+					// Create an empty window
+					window = new Window(text, configuration);
+				}
+			}
+		} catch (Exception e) {
+			try {
+				if (!e.getMessage().equals("")) {
+					JOptionPane.showMessageDialog(window, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(window, "An error has occured", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}
+				// Close application
+				if (window != null) {
+					window.dispose();
+				}
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(window, "An error has occured", "ERROR", JOptionPane.ERROR_MESSAGE);
+				// Close application
+				if (window != null) {
+					window.dispose();
+				}
 			}
 		}
 	}
